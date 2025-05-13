f0822d7ceec4d68d65e50f01b61a12c9e33977c6
Naveen-Th <gowdanavi279@gmail.com>
2025-05-13 21:10:42 +0530

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Suspense, lazy } from "react";
 import DefaultLayout from "./layouts/DefaultLayout";
 import LoaderComponent from "@/components/LoaderComponent";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar"
 
 // Lazy-loaded pages
 const Home = lazy(() => import("./pages/Home"));
@@ -12,14 +13,15 @@ function App() {
   return (
     <>
       <Router>
-        <DefaultLayout>
+        {/* <DefaultLayout> */}
           <Suspense fallback={<LoaderComponent />}>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/corporate" element={<About />} />
+              <Route path="/nav" element={<Navbar />} />
             </Routes>
           </Suspense>
-        </DefaultLayout>
+        {/* </DefaultLayout> */}
       </Router>
     </>
   );
