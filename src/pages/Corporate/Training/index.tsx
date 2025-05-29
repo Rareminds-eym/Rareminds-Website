fae4c3bf70ac2c59afb400033bb7052fdd4af81d
gokulrajrz <rajgokul9847161393@gmail.com>
2025-05-29 15:36:25 +0530

    testimonials, dashboard

--- src/pages/Corporate/Training/index.tsx
+++ src/pages/Corporate/Training/index.tsx
@@ -3,35 +3,31 @@ import FullScreenCarousel from "@/components/Corporate/Recruitment/Home/Carousel
 import BookCall from "@/components/Corporate/Training/BookCall";
 import CaseStudies from "@/components/Corporate/Training/CaseStudies";
 import ContactSection from "@/components/Corporate/Training/Contact/ContactSection";
+import InstitutionDashboardSection from "@/components/Corporate/Training/InstitutionDashboardSection";
 import Numbers from "@/components/Corporate/Training/Numbers";
 import Problem from "@/components/Corporate/Training/Problem";
 import Services from "@/components/Corporate/Training/Services";
 import Solution from "@/components/Corporate/Training/Solution";
 import TestimonialQuotes from "@/components/Corporate/Training/TestimonialQuotes";
+import TestimonialVideos from "@/components/Corporate/Training/TestimonialVideos";
 import WorkWith from "@/components/Corporate/Training/WorkWith";
 import React from "react";
 
 const CorporateTraining: React.FC = () => {
   const slides = [
     {
-      heading:
-        "Your Training ROI Partner",
-      subheading:
-        "We're not just trainers. We're business enablers.",
+      heading: "Your Training ROI Partner",
+      subheading: "We're not just trainers. We're business enablers.",
       img: "/Corporate/Images/Training/hero/training-partner.webp",
     },
     {
-      heading:
-        "Across 25 Sectors. In 13 Languages. With Zero Excuses.",
-      subheading:
-        "",
+      heading: "Across 25 Sectors. In 13 Languages. With Zero Excuses.",
+      subheading: "",
       img: "/Corporate/Images/Home/Hero/OnePartner.webp",
     },
     {
-      heading:
-        "Want Results?",
-      subheading:
-        "We brought down post-training dropouts by 74%. Ask Toyota.",
+      heading: "Want Results?",
+      subheading: "We brought down post-training dropouts by 74%. Ask Toyota.",
       img: "/Corporate/Images/Home/Hero/Time-to-Hire.webp",
     },
   ];
@@ -59,7 +55,13 @@ const CorporateTraining: React.FC = () => {
         <Numbers />
       </div>
       <div className="relative bg-white">
+        <InstitutionDashboardSection />
+      </div>
+      <div className="relative bg-white">
         <WorkWith />
+      </div>
+      <div className="relative bg-white">
+        <TestimonialVideos />
       </div>
       <div className="relative bg-white">
         <BookCall />
@@ -67,7 +69,6 @@ const CorporateTraining: React.FC = () => {
       <div className="relative bg-white">
         <ContactSection />
       </div>
-      <FAQChatbot />
     </>
   );
 };
