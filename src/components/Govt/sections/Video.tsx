97dd8fafbf56c8356f975de2bcfadf055e50e1be
Naveen-250 <intern@rareminds.in>
2025-06-05 16:22:20 +0530

    Update Video.tsx

--- src/components/Govt/sections/Video.tsx
+++ src/components/Govt/sections/Video.tsx
@@ -1,40 +1,40 @@
-import { generateKeySync } from 'crypto';
+
 import { useState, useRef, MouseEvent } from 'react';
 import Modal from 'react-modal';
 
 const contentData = [
   {
-    image: '/public/Govt-Images/ytbanner2.png',
+    image: '/Govt-Images/Videos/Thumbnails/Faculty_testimonail.png',
     video: 'https://youtu.be/LYmob1dwjt8',
     title: 'Education Excellence'
   },
   {
-    image: '/public/Govt-Images/ytbanner1.png',
+    image: '/Govt-Images/Videos/Thumbnails/Fdp.png',
     video: 'https://youtu.be/2BNwt75ExEk',
     title: 'University Campus'
   },
   {
-    image: '/public/Govt-Images/ytbanner3.png',
+    image: '/Govt-Images/Videos/Thumbnails/Faculty_testimonail.png',
     video: 'https://youtu.be/zInPOXMjHpQ',
     title: 'Learning Experience'
   },
   {
-    image: '/public/Govt-Images/ytbanner2.png',
+    image: '/Govt-Images/Videos/Thumbnails/student.png',
     video: 'https://youtu.be/oIeAey8NxmA',
     title: 'Student Life'
   },
   {
-    image: '/public/Govt-Images/ytbanner3.png',
+    image: '/Govt-Images/Videos/Thumbnails/Faculty_testimonail.png',
     video: 'https://youtu.be/w5HmwkOF92E',
     title: 'Modern Classroom'
   },
   {
-    image: '/public/Govt-Images/ytbanner1.png',
+    image: '/Govt-Images/Videos/Thumbnails/Fdp_highlights.png',
     video: 'https://youtu.be/LYmob1dwjt8',
     title: 'Library Resources'
   },
   {
-    image: '/public/Govt-Images/ytbanner3.png',
+    image: '/Govt-Images/Videos/Thumbnails/Fdp_highlights.png',
     video: 'https://youtu.be/Z467zES119Q',
     title: 'Library Resources'
   }
