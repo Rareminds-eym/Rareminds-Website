e4c635d59481e6211bcf35579ebdfa4752f4a40a
gokulrajrz <rajgokul9847161393@gmail.com>
2025-06-14 11:11:57 +0530

    .

---
+++ src/pages/Blogs/index.tsx
@@ -0,0 +1,8 @@
+import BlogListing from "@/components/Govt/Blogs/BlogListing";
+
+const Blogs: React.FC = () => {
+  // BlogListing should use Link or navigate to /blogs/:slug when a blog is selected
+  return <BlogListing />;
+};
+
+export default Blogs;
