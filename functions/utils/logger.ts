f2ef65672ad1b2aea2432142e91aab5859027deb
karthikeyans201219995 <karthikeyans201219995@gmail.com>
2026-07-03 12:59:55 +0530

    fix(logger): remove undefined from LogContext type

--- functions/utils/logger.ts
+++ functions/utils/logger.ts
@@ -6,7 +6,7 @@
 type LogLevel = 'debug' | 'info' | 'warn' | 'error';
 
 interface LogContext {
-  [key: string]: string | number | boolean | null | undefined;
+  [key: string]: string | number | boolean | null;
 }
 class Logger {
   private context: string;
