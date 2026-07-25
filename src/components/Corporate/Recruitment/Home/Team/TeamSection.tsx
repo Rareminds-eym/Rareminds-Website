a6480f4638d0d43e3116ef5421e878c0c9a7612b
Anandh333 <anandhageethan333@gmail.com>
2026-07-25 14:20:48 +0530

    corporate recuirement

--- src/components/Corporate/Recruitment/Home/Team/TeamSection.tsx
+++ src/components/Corporate/Recruitment/Home/Team/TeamSection.tsx
@@ -3,10 +3,10 @@ import { Linkedin, Users } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const missionBubbleClasses =
-  "absolute pt-8 sm:pt-10 md:pt-9 lg:pt-[50px] top-0 px-3 sm:px-6 md:px-6 lg:px-5 w-full h-[150px] sm:h-[170px] md:h-[160px] lg:h-[200px] opacity-0 group-hover:opacity-100 transition-all duration-300 overflow-hidden";
+  "absolute pt-6 sm:pt-10 min-[640px]:pt-8 md:pt-9 lg:pt-[50px] top-0 px-3 min-[520px]:px-6 min-[640px]:px-7 md:px-6 lg:px-5 w-full h-[200px] sm:h-[170px] min-[640px]:h-[150px] md:h-[160px] lg:h-[200px] opacity-0 group-hover:opacity-100 transition-all duration-300 overflow-hidden";
 
 const imageWrapperClasses =
-  "absolute bottom-[-60px] sm:bottom-[-70px] md:bottom-[-65px] lg:bottom-[-100px] scale-100 sm:scale-110 md:scale-105 lg:scale-150";
+  "absolute bottom-0 min-[400px]:bottom-[-25px] min-[520px]:bottom-[-45px] min-[640px]:bottom-[-85px] md:bottom-[-65px] lg:bottom-[-100px] scale-100 min-[400px]:scale-95 min-[520px]:scale-90 min-[640px]:scale-[.85] md:scale-105 lg:scale-150";
 
 const teamMembers = [
   {
@@ -89,13 +89,13 @@ const TeamSection = () => {
               }`}
             >
               <div
-                className={`relative ${member.bg} rounded-full flex flex-col h-[450px] sm:h-[480px] md:h-[460px] lg:h-[500px] overflow-hidden group`}
+                className={`relative ${member.bg} rounded-full flex flex-col h-[350px] min-[400px]:h-[400px] min-[520px]:h-[450px] sm:h-[480px] min-[640px]:h-[440px] md:h-[460px] lg:h-[500px] overflow-hidden group`}
               >
-                <div className="p-5 flex flex-col group-hover:opacity-0 transition-all duration-300">
-                  <p className="text-lg font-bold text-center mt-10">
+                <div className="p-5 min-[640px]:pt-3 flex flex-col group-hover:opacity-0 transition-all duration-300">
+                  <p className="text-base min-[520px]:text-lg min-[640px]:text-base md:text-lg font-bold text-center mt-6 min-[520px]:mt-10 min-[640px]:mt-6">
                     {member.name.toUpperCase()}
                   </p>
-                  <p className="mt-1 text-center">{member.designation}</p>
+                  <p className="mt-1 text-sm min-[520px]:text-base text-center">{member.designation}</p>
                 </div>
 
                 <div className={missionBubbleClasses}>
